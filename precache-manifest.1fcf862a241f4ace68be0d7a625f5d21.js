c439f4c961b85d53d86124847aa781663a4b3a71
barcaayush1 <bhattarai.aayush2@gmail.com>
2020-07-15 16:51:08 -0500

    Updates

---
+++ precache-manifest.1fcf862a241f4ace68be0d7a625f5d21.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50bb06f5569cb09a10debfffd5a3db2f",
+    "url": "/barcaayush1/nirmalbhattarai/index.html"
+  },
+  {
+    "revision": "3f1ebbf47c617fe8b6e4",
+    "url": "/barcaayush1/nirmalbhattarai/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "99d29e16d3a8c497306b",
+    "url": "/barcaayush1/nirmalbhattarai/static/css/main.12b3f6bb.chunk.css"
+  },
+  {
+    "revision": "3f1ebbf47c617fe8b6e4",
+    "url": "/barcaayush1/nirmalbhattarai/static/js/2.9ad97ca9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/barcaayush1/nirmalbhattarai/static/js/2.9ad97ca9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99d29e16d3a8c497306b",
+    "url": "/barcaayush1/nirmalbhattarai/static/js/main.03a6017c.chunk.js"
+  },
+  {
+    "revision": "756d7220b688de81d9e8",
+    "url": "/barcaayush1/nirmalbhattarai/static/js/runtime-main.d1315ea7.js"
+  },
+  {
+    "revision": "2cddedcabed248ba19ecd97c0b44aa25",
+    "url": "/barcaayush1/nirmalbhattarai/static/media/Profile.2cddedca.jpg"
+  }
+]);
